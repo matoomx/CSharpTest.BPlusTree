55d1683be04870d85c9f2877246122684bbdaa7e
Mattias Törnblom <mattias.tornblom@ecoguard.se>
2025-11-06 22:44:08 +0100

    Re Add lurchtable tests

---
+++ Tests/BPlusTreeTests/TestGenericCollection.cs
@@ -0,0 +1,219 @@
+﻿#region Copyright 2011-2014 by Roger Knapp, Licensed under the Apache License, Version 2.0
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion
+
+using System.Collections.Generic;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BPlusTreeTests;
+
+public abstract class TestGenericCollection<TList, TItem> where TList : ICollection<TItem>
+{
+	protected abstract TList CollectionFactory();
+
+	protected abstract TItem[] GetSample();
+
+    protected TList CreateSample(TItem[] items)
+    {
+        TList list = CollectionFactory();
+
+        int count = 0;
+        Assert.HasCount(count, list);
+
+        foreach (TItem item in items)
+        {
+            list.Add(item);
+            Assert.HasCount(++count, list);
+        }
+        return list;
+    }
+
+    [TestMethod]
+    public void TestAddRemove()
+    {
+        TList list = CollectionFactory();
+        TItem[] items = GetSample();
+
+        int count = 0;
+        Assert.HasCount(count, list);
+
+        foreach (TItem item in items)
+        {
+            list.Add(item);
+            Assert.HasCount(++count, list);
+        }
+        foreach (TItem item in items)
+        {
+            Assert.IsTrue(list.Remove(item));
+            Assert.HasCount(--count, list);
+        }
+    }
+
+    [TestMethod]
+    public void TestAddReverseRemove()
+    {
+        TList list = CollectionFactory();
+        TItem[] items = GetSample();
+
+        int count = 0;
+        Assert.HasCount(count, list);
+
+        foreach (TItem item in items)
+        {
+            list.Add(item);
+            Assert.HasCount(++count, list);
+        }
+        for (int ix = items.Length - 1; ix >= 0; ix--)
+        {
+            Assert.IsTrue(list.Remove(items[ix]));
+            Assert.HasCount(--count, list);
+        }
+    }
+
+    [TestMethod]
+    public void TestClear()
+    {
+        TList list = CollectionFactory();
+        TItem[] items = GetSample();
+
+        foreach (TItem item in items)
+            list.Add(item);
+
+        Assert.HasCount(items.Length, list);
+
+        Assert.AreNotEqual(0, list.Count);
+        list.Clear();
+        Assert.IsEmpty(list);
+    }
+
+    [TestMethod]
+    public void TestContains()
+    {
+        TList list = CollectionFactory();
+        TItem[] items = GetSample();
+
+        foreach (TItem item in items)
+            list.Add(item);
+        
+        Assert.HasCount(items.Length, list);
+
+        foreach (TItem item in items)
+            Assert.Contains(item, list);
+    }
+
+    [TestMethod]
+    public void TestCopyTo()
+    {
+        TList list = CollectionFactory();
+        List<TItem> items = new List<TItem>(GetSample());
+
+        foreach (TItem item in items)
+            list.Add(item);
+        
+        Assert.HasCount(items.Count, list);
+
+        TItem[] copy = new TItem[items.Count + 1];
+        list.CopyTo(copy, 1);
+        Assert.AreEqual(default(TItem), copy[0]);
+
+        for (int i = 1; i < copy.Length; i++)
+            Assert.IsTrue(items.Remove(copy[i]));
+
+        Assert.IsEmpty(items);
+    }
+
+    [TestMethod]
+    public void TestIsReadOnly()
+    {
+        Assert.IsFalse(CollectionFactory().IsReadOnly);
+    }
+
+    [TestMethod]
+    public void TestGetEnumerator()
+    {
+        TList list = CollectionFactory();
+        List<TItem> items = new List<TItem>(GetSample());
+
+        foreach (TItem item in items)
+            list.Add(item);
+
+        Assert.HasCount(items.Count, list);
+
+        foreach (TItem item in list)
+            Assert.IsTrue(items.Remove(item));
+
+        Assert.IsEmpty(items);
+    }
+
+    [TestMethod]
+    public void TestGetEnumerator2()
+    {
+        TList list = CollectionFactory();
+        List<TItem> items = new List<TItem>(GetSample());
+
+        foreach (TItem item in items)
+            list.Add(item);
+
+        Assert.HasCount(items.Count, list);
+
+        foreach (TItem item in ((System.Collections.IEnumerable)list))
+            Assert.IsTrue(items.Remove(item));
+
+        Assert.IsEmpty(items);
+    }
+
+    public static void VerifyCollection<T, TC>(IEqualityComparer<T> comparer, ICollection<T> expected, TC collection) where TC : ICollection<T>
+    {
+        Assert.AreEqual(expected.IsReadOnly, collection.IsReadOnly);
+        Assert.HasCount(expected.Count, collection);
+        CompareEnumerations(comparer, expected, collection);
+        using (var a = expected.GetEnumerator())
+        using (var b = collection.GetEnumerator())
+        {
+            bool result;
+            Assert.IsTrue(b.MoveNext());
+            b.Reset();
+            Assert.AreEqual(result = a.MoveNext(), b.MoveNext());
+            while (result)
+            {
+                Assert.IsTrue(comparer.Equals(a.Current, b.Current));
+                Assert.IsTrue(comparer.Equals(a.Current, (T)((System.Collections.IEnumerator)b).Current));
+                Assert.AreEqual(result = a.MoveNext(), b.MoveNext());
+            }
+        }
+
+        T[] items = new T[10 + collection.Count];
+        collection.CopyTo(items, 5);
+        Array.Copy(items, 5, items, 0, collection.Count);
+        Array.Resize(ref items, collection.Count);
+        CompareEnumerations(comparer, expected, collection);
+
+        for( int i=0; i < 5; i++)
+            Assert.Contains(items[i], collection);
+    }
+
+    public static void CompareEnumerations<T>(IEqualityComparer<T> comparer, IEnumerable<T> expected, IEnumerable<T> collection)
+    {
+		using var a = expected.GetEnumerator();
+		using var b = collection.GetEnumerator();
+		bool result;
+		Assert.AreEqual(result = a.MoveNext(), b.MoveNext());
+		while (result)
+		{
+			Assert.IsTrue(comparer.Equals(a.Current, b.Current));
+			Assert.AreEqual(result = a.MoveNext(), b.MoveNext());
+		}
+	}
+}
---
+++ Tests/BPlusTreeTests/TestLurchTableThreading.cs
@@ -0,0 +1,207 @@
+﻿#region Copyright 2012-2014 by Roger Knapp, Licensed under the Apache License, Version 2.0
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using CSharpTest.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BPlusTreeTests;
+
+[TestClass]
+public class TestLurchTableThreading
+{
+    private const int MAXTHREADS = 8;
+    private const int COUNT = 1000;
+    static LurchTable<Guid, T> CreateMap<T>()
+    {
+        var ht = new LurchTable<Guid, T>(COUNT, LurchTableOrder.Access);
+        return ht;
+    }
+
+    private static void Parallel<T>(int loopCount, T[] args, Action<T> task)
+    {
+        var timer = Stopwatch.StartNew();
+        int[] ready = new[] { 0 };
+        ManualResetEvent start = new ManualResetEvent(false);
+        int nthreads = Math.Min(MAXTHREADS, args.Length);
+        var threads = new Thread[nthreads];
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i] = new Thread((ithread) =>
+            {
+                Interlocked.Increment(ref ready[0]);
+                start.WaitOne();
+                for(int loop = 0; loop < loopCount; loop++)
+                    for (int ix = (int)ithread; ix < args.Length; ix += nthreads)
+                        task(args[ix]);
+            });
+        }
+
+        int threadIx = 0;
+        foreach (var t in threads)
+            t.Start(threadIx++);
+
+        while (Interlocked.CompareExchange(ref ready[0], 0, 0) < nthreads)
+            Thread.Sleep(0);
+
+        start.Set();
+
+        foreach (var t in threads)
+            t.Join();
+
+        Trace.TraceInformation("Execution time: {0}", timer.Elapsed);
+    }
+
+    [TestMethod]
+    public void TestLimitedInsert()
+    {
+        var Map = new LurchTable<Guid, bool>(LurchTableOrder.Access, 1000);
+        var ids = CreateSample(Guid.NewGuid(), 1000000);
+
+        Parallel(1, ids, id =>
+        {
+			Assert.IsTrue(Map.TryAdd(id, true));
+			Map.TryGetValue(id, out bool test);
+        });
+
+        Assert.AreEqual(1000, Map.Count);
+    }
+
+    [TestMethod]
+    public void TestInsert()
+    {
+        var Map = CreateMap<bool>();
+        var ids = CreateSample(Guid.NewGuid(), COUNT);
+
+		Parallel(1, ids, id => { Assert.IsTrue(Map.TryAdd(id, true)); });
+
+		Assert.AreEqual(ids.Length, Map.Count);
+        foreach (var id in ids)
+            Assert.IsTrue(Map.TryGetValue(id, out bool test) && test);
+    }
+
+    [TestMethod]
+    public void TestDelete()
+    {
+        var Map = CreateMap<bool>();
+        var ids = CreateSample(Guid.NewGuid(), COUNT);
+        foreach (var id in ids)
+            Assert.IsTrue(Map.TryAdd(id, true));
+
+        bool test;
+        Parallel(1, ids, id => { Assert.IsTrue(Map.Remove(id)); });
+
+        Assert.AreEqual(0, Map.Count);
+        foreach (var id in ids)
+            Assert.IsFalse(Map.TryGetValue(id, out test));
+    }
+
+    [TestMethod]
+    public void TestInsertDelete()
+    {
+        var Map = CreateMap<bool>();
+        var ids = CreateSample(Guid.NewGuid(), COUNT);
+
+        bool test;
+        Parallel(100, ids, id => { Assert.IsTrue(Map.TryAdd(id, true)); Assert.IsTrue(Map.Remove(id)); });
+
+        Assert.AreEqual(0, Map.Count);
+        foreach (var id in ids)
+            Assert.IsFalse(Map.TryGetValue(id, out test));
+    }
+
+    [TestMethod]
+    public void TestInsertUpdateDelete()
+    {
+        var Map = CreateMap<bool>();
+        var ids = CreateSample(Guid.NewGuid(), COUNT);
+
+		Parallel(100, ids, id => 
+        { 
+            Assert.IsTrue(Map.TryAdd(id, true)); Assert.IsTrue(Map.TryUpdate(id, false, true)); 
+            Assert.IsTrue(Map.Remove(id)); 
+        });
+
+		Assert.AreEqual(0, Map.Count);
+        foreach (var id in ids)
+            Assert.IsFalse(Map.TryGetValue(id, out bool test));
+    }
+
+    [TestMethod]
+    public void CompareTest()
+    {
+        const int size = 1000000;
+        int reps = 3;
+        Stopwatch timer;
+
+        IDictionary<Guid, TestValue> dict = new SynchronizedDictionary<Guid,TestValue>(new Dictionary<Guid, TestValue>(size));
+        IDictionary<Guid, TestValue> test = new LurchTable<Guid, TestValue>(size);
+
+        for (int rep = 0; rep < reps; rep++)
+        {
+            var sample = CreateSample(Guid.NewGuid(), size);
+
+            timer = Stopwatch.StartNew();
+            Parallel(1, sample, item => dict.Add(item, new TestValue {Id = item, Count = rep}));
+            Trace.TraceInformation("Dict Add: {0}", timer.Elapsed);
+
+            timer = Stopwatch.StartNew();
+            Parallel(1, sample, item => test.Add(item, new TestValue { Id = item, Count = rep }));
+            Trace.TraceInformation("Test Add: {0}", timer.Elapsed);
+
+            timer = Stopwatch.StartNew();
+            Parallel(1, sample, item => dict[item] = new TestValue { Id = item, Count = rep });
+            Trace.TraceInformation("Dict Update: {0}", timer.Elapsed);
+
+            timer = Stopwatch.StartNew();
+            Parallel(1, sample, item => test[item] = new TestValue { Id = item, Count = rep });
+            Trace.TraceInformation("Test Update: {0}", timer.Elapsed);
+
+            timer = Stopwatch.StartNew();
+            Parallel(1, sample, item => dict.Remove(item));
+            Trace.TraceInformation("Dict Rem: {0}", timer.Elapsed);
+            Assert.IsEmpty(dict);
+
+            timer = Stopwatch.StartNew();
+            Parallel(1, sample, item => test.Remove(item));
+            Trace.TraceInformation("Test Rem: {0}", timer.Elapsed);
+
+            test.Clear();
+            dict.Clear();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+
+    struct TestValue
+    {
+        public Guid Id;
+        public int Count;
+    };
+
+    public static Guid[] CreateSample(Guid seed, int size)
+    {
+        var sample = new Guid[size];
+
+        for (int i = 0; i < size; i++)
+            sample[i] = Guid.NewGuid();
+
+        return sample;
+    }
+}
